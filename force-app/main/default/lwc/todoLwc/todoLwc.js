93ebc4c37a09654dccc6e4b81eb002f07975856a
1432jd <junaid.a@techmatrixconsulting.com>
2023-05-18 17:48:25 +0530

    create LWC Todo list

---
+++ force-app/main/default/lwc/todoLwc/todoLwc.js
@@ -0,0 +1,8 @@
+import { LightningElement, api } from 'lwc';
+import Task_Field from '@salesforce/schema/Todo_List__c.Task__c'
+
+export default class TodoLwc extends LightningElement {
+    handleOnChange(){
+
+    }
+}
