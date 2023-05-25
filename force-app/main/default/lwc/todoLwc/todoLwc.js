5038799305710b09067d49492548544b76c20407
1432jd <junaid.a@techmatrixconsulting.com>
2023-05-25 11:48:31 +0530

    Created TODO

--- force-app/main/default/lwc/todoLwc/todoLwc.js
+++ force-app/main/default/lwc/todoLwc/todoLwc.js
@@ -1,8 +1,54 @@
-import { LightningElement, api } from 'lwc';
-import Task_Field from '@salesforce/schema/Todo_List__c.Task__c'
+import { LightningElement, api , track} from 'lwc';
+import getTodoList from '@salesforce/apex/TodoController.getTodoList'
+import insertTodolist from '@salesforce/apex/TodoController.insertTodolist'
 
 export default class TodoLwc extends LightningElement {
-    handleOnChange(){
+   @track todoList = [];
+    task;
+    connectedCallback(){
+        this.getRecord();
+        // console.log(todoList);
+    }
 
+    getRecord(){
+        getTodoList()
+            .then(result => {
+                this.todoList = result;
+                console.log(result);
+            })
+            .catch(error => {
+                console.log('Error in getRecord : '+error);
+            })
+    }
+
+
+   
+    handleOnChange(event){
+        console.log('>>> handleOnChange',event.target.value);
+        this.task = event.target.value;
+    }
+
+    handleOnClick(){
+        console.log('>>> input');
+        console.log('todo >> list >> ',this.todoList);
+        if(this.task){
+            console.log('>>> text',this.task);
+            this.insertTodolist1();
+            setTimeout(getRecord(), 2000);
+        }
+
+    }
+
+
+
+    insertTodolist1(){
+        console.log("this.task >>> "+this.task);
+        insertTodolist({text : this.task })
+            .then(result => {
+                console.log('result >>> '+result);
+            })
+            .catch(error => {
+                console.log('Error in getRecord : '+JSON.stringify(error));
+            })
     }
 }
