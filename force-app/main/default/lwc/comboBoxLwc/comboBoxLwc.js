06fbb6a665c92e5b82018b8bca5ee49f5f197bdf
1432jd <junaid.a@techmatrixconsulting.com>
2023-05-25 12:03:27 +0530

    update the code

--- force-app/main/default/lwc/comboBoxLwc/comboBoxLwc.js
+++ force-app/main/default/lwc/comboBoxLwc/comboBoxLwc.js
@@ -2,34 +2,31 @@ import { LightningElement , track} from 'lwc';
 import getLastAccount from '@salesforce/apex/MyUtility.getLastAccount';
 
 export default class ComboBoxLwc extends LightningElement {
-    // get options(){
-    //     // return [
-    //     //     { label: 'New', value: 'new' },
-    //     //     { label: 'In Progress', value: 'inProgress' },
-    //     //     { label: 'Finished', value: 'finished' },
-    //     // ];
-
-    //     return listOfAccount;
-    // };
-    value;
-    @track listOfAccount = [];
+    value = "JD";
+    @track listOfAccount;
+    
     connectedCallback(){
         this.getLastAccount();
     }
 
     getLastAccount(){
         getLastAccount()
-            .then(result => {
+        .then(result => {
+                let listAcc = [];
+                this.listOfAccount = [];
                 console.log('list of Account ',result);
                 console.log('list of Account ',result["0015g00000V0JAGAA3"]);
                 // if(result){
                     for(let x in result){
                         // alert('Result key>>'+x);
                         this.listOfAccount.push({label:result[x], value: x});
+                        listAcc.push({label:result[x], value: x});
                     } 
                 // }
                 // listOfAccount = result;
-                console.log('Account List  ',this.listOfAccount.length);
+                console.log('Account List  ',this.listOfAccount);
+                console.log('Account List  ',listAcc.push);
+                // this.listOfAccount = listAcc;
             })
             .catch(error => {
                 console.log('Error in getRecord : '+error);
