1b3e836390dbb5b292b9ee76efa6811c04f67a1c
1432junaid <1432junaid@gmail.com>
2024-04-09 01:01:15 +0530

    update record Edit form

--- force-app/main/default/lwc/recordEditForm/recordEditForm.js
+++ force-app/main/default/lwc/recordEditForm/recordEditForm.js
@@ -1,12 +1,11 @@
 import { LightningElement , api} from 'lwc';
-import PizzaName from '@salesforce/schema/Pizza_Menu__c.Name';
-import PizzaPrice from '@salesforce/schema/Pizza_Menu__c.Pizza_Price__c';
-import PizzaQuality from '@salesforce/schema/Pizza_Menu__c.Pizza_Quantity__c';
+import Pizza_Price_Field from '@salesforce/schema/Pizza_Menu__c.Pizza_Price__c'
+import Pizza_Quantity_Field from '@salesforce/schema/Pizza_Menu__c.Pizza_Quantity__c'
 
 export default class RecordEditForm extends LightningElement {
-    PizzaName = PizzaName;
-    PizzaPrice = PizzaPrice;
-    PizzaQuality = PizzaQuality;
     @api recordId;
     @api objectApiName;
-    }
+    pizzaPrice = Pizza_Price_Field;
+    pizzaQuality = Pizza_Quantity_Field;
+    // fields = [Pizza_Price_Field, Pizza_Quantity_Field];
+}
